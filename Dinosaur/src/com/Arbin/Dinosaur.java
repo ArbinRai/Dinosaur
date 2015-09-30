28ab4f0fa5d00793dd80b4afc43de7ac6a9344d1
Arbin <Arbinkrai@gmail.com>
2015-09-30 14:17:54 -0500

    Created new Dinosaur diplodocus

--- Dinosaur/src/com/Arbin/Dinosaur.java
+++ Dinosaur/src/com/Arbin/Dinosaur.java
@@ -4,6 +4,8 @@ package com.Arbin;
  * Created by lt3733yt on 9/28/2015.
  */
 public class Dinosaur {
+    String name;
+    String color;
     public void roar(){
         System.out.println("GRRR!!!!!");
     }
--- Dinosaur/src/com/Arbin/DinosaurSupervisor.java
+++ Dinosaur/src/com/Arbin/DinosaurSupervisor.java
@@ -7,8 +7,19 @@ public class DinosaurSupervisor {
         Dinosaur velociraptor = new Dinosaur();
         Dinosaur diplodocus=new Dinosaur();
         t_rex.roar();
+        t_rex.color="red";
+        t_rex.name="tyrannosaurus Rex";
+        System.out.println("The name of this dinosaur is "+t_rex.name);
+        System.out.println("It's color is "+t_rex.color);
         velociraptor.roar();
+        velociraptor.color="blue";
+        velociraptor.name="Velociraptor";
+        System.out.println("The name of this dinosaur is "+velociraptor.name);
+        System.out.println("It's color is "+velociraptor.color);
         diplodocus.roar();
+        diplodocus.color="green";
+        diplodocus.name="Diplodocus";
+        System.out.println("The name of this dinosaur is "+diplodocus.name);
+        System.out.println("It's color is "+diplodocus.color);
     }
 }
-
